3122c61568925e356bef5744a16902e926864e8d
Paulo H N Matos <paulophdev@gmail.com>
2025-03-30 22:17:41 -0300

    Adicionar overlay ao modal de teste de conexão

--- resources/js/components/connection-list/test-message-modal/TestMessageModal.vue
+++ resources/js/components/connection-list/test-message-modal/TestMessageModal.vue
@@ -1,7 +1,7 @@
 <!-- resources/js/components/connection-list/test-message-modal/TestMessageModal.vue -->
 <script setup lang="ts">
-import { DialogRoot, DialogContent, DialogTitle, DialogDescription, DialogClose } from 'radix-vue';
-import { X, Phone, MessageSquare } from 'lucide-vue-next'; // Adicionar ícones Phone e MessageSquare
+import { DialogRoot, DialogContent, DialogTitle, DialogDescription, DialogClose, DialogOverlay } from 'radix-vue';
+import { X, Phone, MessageSquare } from 'lucide-vue-next';
 import { ref } from 'vue';
 
 defineProps<{
@@ -38,6 +38,7 @@ const closeModal = () => {
 
 <template>
   <DialogRoot :open="open" @update:open="emit('update:open', $event)">
+    <DialogOverlay class="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0 z-30" />
     <DialogContent
       class="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[20px] bg-white p-[30px] shadow-[hsl(206_22%7%/_35%)_0px_10px_38px_-10px,_hsl(206_22%7%/_20%)_0px_10px_20px_-15px] focus:outline-none z-[100] font-sans"
     >
