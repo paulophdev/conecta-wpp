ed120601aae2f544511e5eb0ae89981c98a27ffc
Paulo H N Matos <paulophdev@gmail.com>
2025-04-20 18:07:50 -0300

    Ajustar nome da página

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -3,7 +3,7 @@ import { Head, Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Welcome">
+    <Head title="Ultra rápido e eficiente!">
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
