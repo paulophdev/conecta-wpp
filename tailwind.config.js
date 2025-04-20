cee56e5c2f418c5a0f8c2dd91c28ca8dc7a61cfa
Paulo H N Matos <paulophdev@gmail.com>
2025-04-20 18:57:27 -0300

    Ajustar font do site

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Instrument Sans', ...defaultTheme.fontFamily.sans],
+                sans: ['Play', ...defaultTheme.fontFamily.sans],
             },
             borderRadius: {
                 lg: 'var(--radius)',
